da2cbe50660f9d7f87c594ea4fb449a43dd4625f
NAGAPRAJWAL4NI20CS057 <108364979+NAGAPRAJWAL4NI20CS057@users.noreply.github.com>
2022-06-29 00:42:15 +0530

    Add files via upload

---
+++ Bipartite.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int M,N; 
+int Graph[10][10];
+bool bpm(int Graph[10][10], int u,bool seen[], int matchR[]){
+    for (int v = 0; v < N; v++){     
+        if (Graph[u][v] && !seen[v]){            
+            seen[v] = true;           
+            if (matchR[v] < 0 || bpm(Graph, matchR[v],seen, matchR)){
+                matchR[v] = u;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+int maxBPM(int Graph[10][10]){
+    int matchR[N]; 
+    memset(matchR, -1, sizeof(matchR));    
+    int result = 0;
+    for (int u = 0; u < M; u++)
+    {        
+        bool seen[N];
+        memset(seen, 0, sizeof(seen));        
+        if (bpm(Graph, u, seen, matchR))
+        {
+            result++;
+        }
+    }
+    return result;
+}
+int main(){
+    cout << "Enter the number of elements in the first and the second set" << endl;
+    cin >> M >> N;
+    cout << "Enter the matrix" << endl;
+    for(int i = 0;i < M;i++){
+        for(int j = 0;j < N;j++){
+            cin >> Graph[i][j];
+        }
+    }                  
+    cout << "The maximum matching possible is " << maxBPM(Graph);
+    return 0;
+} 
